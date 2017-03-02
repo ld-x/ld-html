236e2d3b3556afc25e35e22e86af4dbfc83f15dc
Steven Martin <stevenisekimartin@gmail.com>
2017-03-03 00:28:46 +1100

    Clean up

--- src/html/HtmlModal.js
+++ src/html/HtmlModal.js
@@ -44,49 +44,69 @@ export default class HtmlModal extends Component {
     const {code} = this.state
 
     return (
-      <Wrapper style={{ width: `700px`, left: `-200px` }}>
-        <EditColumn className='ld-edit-html-wrapper'>
-          <EditTextArea
-            className='ld-edit-html-textarea'
-            ref='textArea'
-            onChange={::this.onEditChange}
-            value={code}
-            onKeyDown={::this.onKeyDown} />
-        </EditColumn>
+      <Wrapper>
+        <HtmlEdit>
+          <EditColumn className='ld-edit-html-wrapper'>
+            <EditTextArea
+              className='ld-edit-html-textarea'
+              ref='textArea'
+              onChange={::this.onEditChange}
+              value={code}
+              onKeyDown={::this.onKeyDown} />
+          </EditColumn>
 
-        <EditActionsColumn className='ld-edit-actions-wrapper'>
-          <EditButton
-            className='ld-edit-html-submit-button'
-            onClick={::this.submitHtml}
-            type='button'
-          >
-          <svg fill='currentColor' height='24' viewBox='0 0 24 24' width='24'>
-              <path d='M0 0h24v24H0z' fill='none' />
-              <path d='M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z' />
-          </svg>
-          </EditButton>
+          <EditActionsColumn className='ld-edit-actions-wrapper'>
+            <EditButton
+              className='ld-edit-html-submit-button'
+              onClick={::this.submitHtml}
+              type='button'
+            >
+            <svg fill='currentColor' height='24' viewBox='0 0 24 24' width='24'>
+                <path d='M0 0h24v24H0z' fill='none' />
+                <path d='M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z' />
+            </svg>
+            </EditButton>
 
-          <EditButton
-            className='ld-edit-html-close-button'
-            onClick={this.props.closeModal}
-            type='button'
-          >
-          <svg width='24' height='24' viewBox='0 0 24 24' className='ld-button-close'>
-            <g fill='currentColor' fillRule='evenodd'>
-              <path d='M16.95 5.636l1.414 1.414L7.05 18.364 5.636 16.95z' />
-              <path d='M16.95 18.364l1.414-1.414L7.05 5.636 5.636 7.05z' />
-            </g>
-          </svg>
-          </EditButton>
-        </EditActionsColumn>
+            <EditButton
+              className='ld-edit-html-close-button'
+              onClick={this.props.closeModal}
+              type='button'
+            >
+            <svg width='24' height='24' viewBox='0 0 24 24' className='ld-button-close'>
+              <g fill='currentColor' fillRule='evenodd'>
+                <path d='M16.95 5.636l1.414 1.414L7.05 18.364 5.636 16.95z' />
+                <path d='M16.95 18.364l1.414-1.414L7.05 5.636 5.636 7.05z' />
+              </g>
+            </svg>
+            </EditButton>
+          </EditActionsColumn>
+        </HtmlEdit>
       </Wrapper>
     )
   }
 }
 
 const Wrapper = styled.div`
+  position: relative;
+  background-color: inherit;
+  width: 700px;
   display: flex;
-  width: 500px;
+`
+
+
+const HtmlEdit = styled.div`
+  position: absolute;
+  transition: opacity 300ms linear;
+  border: 1px solid #F1F1F1;
+  border-radius: 2px;
+  background-color: inherit;
+  box-shadow: 0 1px 18px 0 rgba(0, 0, 0, 0.3);
+  width: 100%;
+  display: flex;
+  overflow-y: scroll;
+  z-index: 100;
+  padding: 1rem;
+  margin-top: -3rem;
 `
 
 const EditColumn = styled.div`
